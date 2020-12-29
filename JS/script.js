98d71547231a3521c850979b33e260f020bbaaf6
Karolina Sobkowicz <karolina.klapyta@gmail.com>
2020-12-29 08:46:45 +0100

    while loop added

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,21 @@
 'use strict';
 
+// Petla for
+
 for (let i = 200; i >= 1; i--) {
     if (i % 2 === 0) {
         document.querySelector('#box').innerHTML += `${i} <br>`;
     }
 }
+
+// Petla while 
+
+let j = 200;
+
+while (j >= 1){
+    if (j % 2 === 0) {
+        document.querySelector('#box2').innerHTML += `${j} <br>`;
+    }
+    j--;
+}
+
